457aa8bc64a2fc145bc7ff3dff2bd5a60b3699eb
beginner <121371582+LSH160981@users.noreply.github.com>
2025-02-20 00:14:23 +0800

    Update more_in_one.sh

--- more_in_one.sh
+++ more_in_one.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# tnt3
+# tnt3 + grass
 wget -O duokai.sh https://raw.githubusercontent.com/LSH160981/Titan-Network/main/duokai.sh && chmod +x duokai.sh && ./duokai.sh -c 5 -g 10
 
 # tm
@@ -12,3 +12,5 @@ docker run -d --restart=always --name repocket -e RP_EMAIL=q2326426@gmail.com -e
 # web
 # docker run -d --name web -it --shm-size=512m -p 6901:6901 -e VNC_PW=Wsh96aGSa156s5 kasmweb/chrome:1.14.0
 
+# earn.fm
+docker run -d --restart=always -e EARNFM_TOKEN="f8978872-6d7c-4b84-9ae3-fb71383e5909" --name earnfm earnfm/earnfm-client:latest

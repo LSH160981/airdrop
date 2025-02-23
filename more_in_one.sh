7d944eddeb51a6e661124234862af1764bf27a87
beginner <121371582+LSH160981@users.noreply.github.com>
2025-02-23 20:36:30 +0800

    Update more_in_one.sh

--- more_in_one.sh
+++ more_in_one.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget -qO- get.docker.com | bash
+
 # tnt3 
 wget -O duokai.sh https://raw.githubusercontent.com/LSH160981/Titan-Network/main/duokai.sh && chmod +x duokai.sh && ./duokai.sh -c 1 -g 21
 

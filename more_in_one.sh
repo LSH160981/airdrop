a98201b931367741b8ef26c236216ccd8ae4d99e
beginner <121371582+LSH160981@users.noreply.github.com>
2026-02-03 17:53:31 +0800

    Update more_in_one.sh

--- more_in_one.sh
+++ more_in_one.sh
@@ -2,7 +2,7 @@
 
 wget -qO- get.docker.com | bash
 
-# tnt3 proxylite packetshare packetsdk
+# tnt3 repocket antgain wizardgain  packetsdk
 wget -O duokai.sh https://raw.githubusercontent.com/LSH160981/Titan-Network/main/duokai.sh && chmod +x duokai.sh && ./duokai.sh
 
 # tm
